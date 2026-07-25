5de07fea12a5d984107b07b46cfda6fd47d73864
Joshua <RanchMonster06@outlook.com>
2026-07-24 21:29:09 -0500

    feat: Created a basic layout for how I think the protocol will work.
    None of this is implemented yet and Is highly subject to change at any time until a stable version is released.

---
+++ src/protocol.rs
@@ -0,0 +1,154 @@
+mod error;
+pub use error::{Error, Result};
+use mdns_sd::{IntoTxtProperties, Receiver, ServiceDaemon, ServiceEvent, ServiceInfo};
+use std::{
+    collections::HashMap,
+    sync::{Arc, LazyLock},
+};
+use tokio::{
+    net::ToSocketAddrs,
+    sync::Mutex,
+    task::{self, JoinHandle},
+};
+
+use crate::sync_logic::SyncTree;
+const SERVICE_TYPE: &str = "_fsync._tcp.local.";
+const DEFAULT_PORT: u16 = 43127;
+const VERSION_KEY_PROPERTY: &str = "version";
+const VERSION_NUMBER: &str = env!("CARGO_PKG_VERSION");
+/// Represents a peer in the network this is mostly like not the actual peer but is a placeholder
+/// until I have figure out what I actual need to know about a peer
+#[derive(Clone)]
+pub struct Peer {
+    version: String,
+    address: String,
+    port: u16,
+}
+impl Peer {
+    pub fn compatible(&self) -> bool {
+        todo!("check if the peer is compatible")
+    }
+    pub fn version(&self) -> &str {
+        &self.version
+    }
+    pub fn address(&self) -> &str {
+        &self.address
+    }
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+    pub fn is_local(&self) -> bool {
+        todo!("check if the peer is local")
+    }
+}
+macro_rules! unwrap_or_create_daemon {
+    ($daemon:expr) => {
+        match $daemon {
+            Some(daemon) => daemon,
+            None => ServiceDaemon::new().expect("Failed to create mdns daemon"),
+        }
+    };
+}
+pub struct ServiceScanner<'daemon> {
+    inner: Receiver<ServiceEvent>,
+    daemon: &'daemon ServiceDaemon,
+}
+impl<'daemon> ServiceScanner<'daemon> {
+    pub async fn next_peer(&mut self) -> Option<Peer> {
+        let event = self.inner.recv_async().await.ok()?;
+        todo!("implement next peer")
+    }
+}
+struct Service {
+    daemon: Option<ServiceDaemon>,
+    peers: Arc<Mutex<Vec<Peer>>>,
+    join_handle: Option<JoinHandle<()>>,
+}
+impl Service {
+    pub async fn new() -> Self {
+        Self {
+            daemon: None,
+            peers: Arc::new(Mutex::new(Vec::new())),
+            join_handle: None,
+        }
+    }
+    pub async fn stop(&mut self) -> Result<()> {
+        self.stop_advertising().await?;
+        if let Some(join_handle) = self.join_handle.take() {
+            join_handle.abort();
+        }
+        self.peers.lock().await.clear();
+        Ok(())
+    }
+    pub async fn connected(&self) -> Vec<Peer> {
+        (*self.peers.lock().await).clone()
+    }
+    pub async fn start(&mut self, trees: Vec<SyncTree>) -> Result<()> {
+        task::spawn(async move { todo!("start the service") });
+        Ok(())
+    }
+    pub async fn discover_peers(&mut self) -> Result<ServiceScanner> {
+        let daemon = self.daemon.take();
+        let result = task::spawn_blocking(move || {
+            let mdns = unwrap_or_create_daemon!(daemon);
+            let receiver = mdns.browse(SERVICE_TYPE)?;
+            Ok::<_, Error>((receiver, mdns))
+        })
+        .await;
+        let (receiver, mdns) =
+            result.expect("Failed to start peer discovery service state damaged")?;
+        self.daemon = Some(mdns);
+        let daemon = self.daemon.as_ref().unwrap();
+        let scanner = ServiceScanner {
+            inner: receiver,
+            daemon,
+        };
+        Ok(scanner)
+    }
+    pub async fn advertise(&mut self) -> Result<()> {
+        let daemon = self.daemon.take();
+        let mdns = task::spawn_blocking(move || {
+            let mdns = unwrap_or_create_daemon!(daemon);
+            let mut hostname = std::env::var("HOSTNAME").unwrap_or_else(|_| {
+                hostname::get()
+                    .expect("Failed to get hostname")
+                    .to_string_lossy()
+                    .to_string()
+            });
+            if !hostname.len() > 15 {
+                tracing::warn!(
+                    "Hostname is too long to advertise and will be truncated to 15 characters"
+                );
+                hostname.truncate(15);
+            }
+            // load the address from the environment variable or default to auto assigned
+            let address = std::env::var("ADDRESS").unwrap_or_else(|_| "0.0.0.0".to_string());
+            let port = std::env::var("PORT")
+                .ok()
+                .and_then(|val| val.parse::<u16>().ok())
+                .unwrap_or(DEFAULT_PORT);
+            let service_info = ServiceInfo::new(
+                SERVICE_TYPE,
+                &hostname,
+                format!("{hostname}.local").as_str(),
+                address,
+                port,
+                [(VERSION_KEY_PROPERTY, VERSION_NUMBER)].as_ref(),
+            )?
+            .enable_addr_auto();
+            mdns.register(service_info)?;
+
+            Ok::<_, Error>(mdns)
+        })
+        .await;
+        let mdns = mdns.expect("Failed to start advertising service state damaged")?;
+        self.daemon = Some(mdns);
+        Ok(())
+    }
+    async fn stop_advertising(&mut self) -> Result<()> {
+        if let Some(daemon) = &mut self.daemon {
+            daemon.unregister(SERVICE_TYPE)?;
+        }
+        Ok(())
+    }
+}
---
+++ src/protocol/error.rs
@@ -0,0 +1,21 @@
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum Error {
+    /// Failed to start the service
+    #[error("Failed to start the service")]
+    ServiceStartError,
+    /// Failed to stop the service
+    #[error("Failed to stop the service gracefully")]
+    ServiceStopError,
+    #[error(
+        "Local add and discovery error please ensure you are not running another instance of fsync or that this device is capable of being discovered"
+    )]
+    MdnsErro(#[from] mdns_sd::Error),
+    /// Peer not found
+    #[error("Peer not found")]
+    PeerNotFound,
+    /// Rejected by a fellow peer
+    #[error("Rejected by a fellow peer")]
+    PeerRejected,
+}
+pub type Result<T> = std::result::Result<T, Error>;
