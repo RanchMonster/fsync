0b139ea7822db9da17c844825c865b27dfecb7c6
Joshua <RanchMonster06@outlook.com>
2026-07-25 20:15:03 -0500

    refactor:removed un nessesary comment

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use blake3::Hash;
 mod fs_watcher;
 mod protocol;
 use fs_watcher::WatcherThread;
-// mod protocol;
 #[tokio::main]
 async fn main() {}
 pub async fn hash_file(path: &Path) -> Result<Hash, std::io::Error> {
