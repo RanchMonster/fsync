f8f666f3245ed0eaf4155e46b600696743a4dbf4
J-Kistner <jk.gm.act@gmail.com>
2026-07-26 22:57:58 -0400

    fix: restructure of test specific config dir
    avoids a remarkably small runtime cost
    
    Co-authored-by: Joshua Tucker <126041686+RanchMonster@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,21 @@ use std::{
 /// The directory where the configuration files are stored.
 /// Also handles creating the directory if it doesn't exist.
 pub static CONFIG_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
-   if cfg!(test) {
+   #[cfg(test)]
+   {
       let path = temp_dir().join(".fsync");
       if !path.exists() {
          create_dir_all(&path).expect("Failed to create test config dir.");
       }
-      path
-   } else {
+      return path;
+      }
+   #[cfg(not(test))]
+   {
       let path = dirs::home_dir().unwrap().join(".fsync");
       if !path.exists() {
          create_dir_all(&path).expect("Failed to create config dir.");
       }
-      path
+      return path;
    }
 });
 
