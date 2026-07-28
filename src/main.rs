5ce8541b920c86e05724cfcfe8424bd162d4b4da
J-Kistner <jk.gm.act@gmail.com>
2026-07-27 20:48:04 -0400

    Ed25519 keypair gen, storage +tests (#4)
    * crate: +rcgen, +dirs,
    
    * feat: `CONFIG_DIR` global
    includes a seporate location for tests
    
    * feat: basic implementation of Ed25519 keypair generation +tests
    
    * fix: restructure of test specific config dir
    
    avoids a remarkably small runtime cost
    
    Co-authored-by: Joshua Tucker <126041686+RanchMonster@users.noreply.github.com>
    
    * fix: added a assert message to keypair alg check
    
    * fix: moved a test only function to the test module
    
    * fix: added safety checks to unix only file protection
    
    ---------
    
    Co-authored-by: Joshua Tucker <126041686+RanchMonster@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use std::path::{Path, PathBuf};
-
 use blake3::Hash;
 mod fs_watcher;
+mod keys;
 mod protocol;
+
 use fs_watcher::WatcherThread;
+use std::{
+   env::temp_dir,
+   fs::create_dir_all,
+   path::{Path, PathBuf},
+   sync::LazyLock,
+};
+
+/// The directory where the configuration files are stored.
+/// Also handles creating the directory if it doesn't exist.
+pub static CONFIG_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
+   #[cfg(test)]
+   {
+      let path = temp_dir().join(".fsync");
+      if !path.exists() {
+         create_dir_all(&path).expect("Failed to create test config dir.");
+      }
+      return path;
+      }
+   #[cfg(not(test))]
+   {
+      let path = dirs::home_dir().unwrap().join(".fsync");
+      if !path.exists() {
+         create_dir_all(&path).expect("Failed to create config dir.");
+      }
+      return path;
+   }
+});
+
 #[tokio::main]
 async fn main() {}
+
 pub async fn hash_file(path: &Path) -> Result<Hash, std::io::Error> {
-    todo!("hash the file")
+   todo!("hash the file")
 }
