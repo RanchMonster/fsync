24cb7da2f02e63664ca48a1c430603720dba2fd6
Joshua <RanchMonster06@outlook.com>
2026-07-31 13:33:26 -0500

    chore:add close codes for the protocol.
    I just need custom close codes for the protocol so I started adding them
    here we can change them later if we need to as long as we only do it
    before first release

--- src/protocol/error.rs
+++ src/protocol/error.rs
@@ -1,33 +1,63 @@
+use quinn::{ConnectionError, ReadError, VarInt, WriteError};
 use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
-   /// Failed to start the service
-   #[error("Failed to start the service")]
-   ServiceStart,
-   /// Failed to stop the service
-   #[error("Failed to stop the service gracefully")]
-   ServiceStop,
    #[error(
       "Local add and discovery error please ensure you are not running another instance of fsync or that this device is capable of being discovered"
    )]
    Mdns(#[from] mdns_sd::Error),
-   /// Peer not found
-   #[error("Peer not found")]
-   PeerNotFound,
    /// Peer was rejected due to a reason
-   #[error("Peer was rejected due to {}", .0)]
+   #[error("Peer was rejected due to {0}")]
    PeerRejected(String),
-   /// Malformed peer
-   #[error("Invalid peer information")]
-   InvalidPeer(mdns_sd::ResolvedService),
    /// Io error
    #[error("IO error")]
    Io(#[from] std::io::Error),
-   #[error("QUIC error")]
-   Quic(#[from] quinn::ConnectionError),
-   #[error("Invalid key hash")]
-   InvalidKeyHash,
+   #[error("QUIC error {0}")]
+   Quic(String),
    #[error("Certificate error: {0}")]
    Rcgen(#[from] rcgen::Error),
+   #[error("Parse data error: {0}")]
+   ParseData(String),
+}
+impl Error {
+   pub const fn close_code(&self) -> Option<CloseCode> {
+      match self {
+         Error::PeerRejected(_) => Some(CloseCode::AuthenticationFailure),
+         Error::Io(_) => Some(CloseCode::InternalError),
+         Error::Quic(_) => Some(CloseCode::InternalError),
+         Error::Rcgen(_) => Some(CloseCode::InternalError),
+         Error::ParseData(_) => Some(CloseCode::InternalError),
+         _ => None,
+      }
+   }
+}
+impl From<ReadError> for Error {
+   fn from(err: quinn::ReadError) -> Self {
+      Error::Quic(err.to_string())
+   }
+}
+impl From<WriteError> for Error {
+   fn from(err: quinn::WriteError) -> Self {
+      Error::Quic(err.to_string())
+   }
+}
+impl From<ConnectionError> for Error {
+   fn from(err: ConnectionError) -> Self {
+      Error::Quic(err.to_string())
+   }
+}
+#[repr(u32)]
+pub enum CloseCode {
+   InvalidProtocol = 1,
+   HandshakeFailure = 2,
+   InternalError = 3,
+   Shutdown = 4,
+   AuthenticationFailure = 5,
+   // add more as needed
+}
+impl Into<VarInt> for CloseCode {
+   fn into(self) -> VarInt {
+      VarInt::from_u32(self as u32)
+   }
 }
 pub type Result<T> = std::result::Result<T, Error>;
