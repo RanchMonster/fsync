4bbdee6f5f61fbf13e4bd9b561f55d0ba814d0bd
Jacob <jk.gm.act@gmail.com>
2026-08-30 13:10:51 +0000

    feat: function to discover and build ignore files

--- src/sync.rs
+++ src/sync.rs
@@ -1,9 +1,15 @@
-use std::time::SystemTime;
+use std::{
+   path::{Path, PathBuf},
+   time::SystemTime,
+};
+
+use ignore::gitignore::{Gitignore as IgnoreFile, GitignoreBuilder as IgnoreBuilder};
 
 use async_trait::async_trait;
 use blake3::Hash;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
 pub trait SyncError: std::error::Error + Send + Sync {}
 
 #[derive(Error, Debug)]
@@ -46,4 +52,31 @@ pub struct Event {
 #[async_trait]
 pub trait EventStream<E: SyncError> {
    async fn next(&mut self) -> Option<Result<Event, E>>;
+}
+
+/// Helper function to get the ignore file for a given synced location
+/// This will look for a .fsyncignore file, then a .ignore file, then a .gitignore file
+/// If none of these files exist, an error will be returned
+fn get_ingnore_file(synced_location: PathBuf, allow_gitignore: bool) -> Option<IgnoreFile> {
+   assert!(synced_location.exists(), "Synced location does not exist");
+
+   fn build_ignore(path: PathBuf) -> Option<IgnoreFile> {
+      if !path.exists() {
+         return None;
+      }
+
+      Some(
+         IgnoreBuilder::new(&path)
+            .build()
+            .unwrap_or_else(|err| panic!("Failed to build ignore file {}: {err}", path.display())),
+      )
+   }
+
+   build_ignore(synced_location.join(".fsyncignore"))
+      .or_else(|| build_ignore(synced_location.join(".ignore")))
+      .or_else(|| {
+         allow_gitignore
+            .then(|| synced_location.join(".gitignore"))
+            .and_then(build_ignore)
+      })
 }
