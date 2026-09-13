6970ccdd5486caf796a9067c1d471eb5696bef7e
Joshua <RanchMonster06@outlook.com>
2026-09-12 20:33:13 -0500

    refactor: removed a lot of redundant code from the config file due to recent changes.

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,10 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Error as IoError, Read, Seek, Write};
 use std::num::NonZeroUsize;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::str::FromStr;
-use std::sync::Arc;
 
-use argon2::password_hash::PasswordHashString;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tracing::instrument;
 
@@ -15,18 +13,6 @@ use crate::CONFIG_DIR;
 
 const FILE_START_POSITION: u64 = 0;
 
-const DEFAULT_CONFIG: &str = r#"
-        # The address to listen on
-        address = "0.0.0.0"
-        # The port to listen on
-        port = 43127
-        # The directories to sync (name -> path)
-        sync_dirs = { }
-        # The pairing mode to use (relaxed, strict, password, keyonly)
-        pair_mode = "RELAXED"
-        # The hostname to advertise
-        hostname = "fsync"
-    "#;
 #[derive(Error, Debug)]
 pub enum ConfigError {
    #[error("Failed to read config file {0}")]
@@ -74,33 +60,33 @@ fn default_workers() -> NonZeroUsize {
    std::thread::available_parallelism().expect("Failed to get the number of available cores")
 }
 
-#[derive(Debug, Deserialize)]
-struct ConfigIntermediater {
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Config {
    #[serde(default = "default_port")]
-   port: u16,
+   pub port: u16,
 
    #[serde(default = "default_address")]
-   address: String,
-
+   pub address: String,
 
    #[serde(default = "default_hostname")]
-   hostname: String,
+   pub hostname: String,
+
+   pub sync_dirs: HashMap<String, PathBuf>,
 
    #[serde(default = "default_workers")]
-   workers: NonZeroUsize,
-
-   sync_dirs: HashMap<String, PathBuf>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Config {
-   pub port: u16,
-   pub address: String,
-   pub hostname: String,
-   pub sync_dirs: HashMap<String, PathBuf>,
    pub workers: NonZeroUsize,
 }
-
+impl Default for Config {
+   fn default() -> Self {
+      Self {
+         port: default_port(),
+         address: default_address(),
+         hostname: default_hostname(),
+         sync_dirs: HashMap::new(),
+         workers: default_workers(),
+      }
+   }
+}
 impl Config {
    /// Reads the config file from the given directory
    /// # Preconditions:
@@ -147,22 +133,7 @@ impl Config {
          .read_to_string(&mut config_contents)
          .map_err(FailedToReadConfig)?;
 
-      let ConfigIntermediater {
-         port,
-         address,
-         hostname,
-         workers,
-         sync_dirs,
-      } = toml::from_str(&config_contents).map_err(Toml)?;
-
-
-      Ok(Config {
-         port,
-         address,
-         hostname,
-         workers,
-         sync_dirs,
-      })
+      toml::from_str(&config_contents).map_err(Toml)
    }
 
    /// Creates the default config file in the given directory
@@ -201,8 +172,11 @@ impl Config {
       assert!(!config_file_path.exists(), "Config file already exists");
 
       let mut config_file = File::create(&config_file_path).map_err(FailedToCreateConfig)?;
+      let default_config =
+         toml::to_string(&Config::default()).expect("Failed to serialize default config");
+
       config_file
-         .write_all(DEFAULT_CONFIG.as_bytes())
+         .write_all(default_config.as_bytes())
          .map_err(FailedToCreateConfig)?;
 
       let config = Self::load().expect("Failed to load config we just created");
@@ -215,18 +189,12 @@ impl Config {
 mod tests {
    use super::*;
    use std::fs;
-   fn setup_config_dir() -> PathBuf {
-      let path = std::env::temp_dir().join("fsync_test_config_dir");
-      unsafe { std::env::set_var("FSYNC_CONFIG_DIR", path.clone()) };
-      let _ = fs::remove_dir_all(&path); // If the directory exists it will be removed
-      path
-   }
+
    #[test]
    fn test_default_config_creation_and_reading() {
-      setup_config_dir();
       let config = Config::create_default_config().expect("Failed to create default config");
       assert_eq!(config.port, 43127);
       assert_eq!(config.address, "0.0.0.0");
-      assert_eq!(config.hostname, "fsync");
+      assert_eq!(config.hostname, hostname::get().unwrap().to_string_lossy());
    }
 }
