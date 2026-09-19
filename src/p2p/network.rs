d5dd7284a1ae44faca27b1bf01bf8c2b71f0a3c2
RanchMonster <ranchmonster06@outlook.com>
2026-09-18 22:01:01 -0500

    chore: expanded the network trait requirements to be more thread safe.
    It was becoming increasingly difficult to work around the very generic
    nature of the network trait. Basically, the network traits are Send and
    Sync so they can be used in async context without extra effort. Now this
    might bite us in the future as it will require us to ensure that
    everything that implements the network related traits is Send and
    Sync, but we will cross that bridge when we get there.

--- src/p2p/network.rs
+++ src/p2p/network.rs
@@ -1,9 +1,11 @@
 pub mod local;
+use std::fmt::Display;
+
 use quinn::{Connecting, Endpoint};
 
 use crate::p2p::PeerId;
 
-pub trait NetworkMember {
+pub trait NetworkMember: Send + Sync + 'static {
    fn id(&self) -> PeerId;
    fn name(&self) -> &str;
 }
@@ -12,10 +14,11 @@ pub trait NetworkMember {
 pub trait NetworkError: std::error::Error + Send + Sync + 'static {}
 impl<T> NetworkError for T where T: std::error::Error + Send + Sync + 'static {}
 
-pub trait Network<Member: NetworkMember, Error: NetworkError> {
+pub trait Network<Member: NetworkMember, Error: NetworkError>: Send + Sync + 'static {
    fn advertise(&mut self) -> Result<(), Error>;
    fn stop_advertising(&mut self) -> Result<(), Error>;
    async fn connect(&self, endpoint: &Endpoint, member: Member) -> Result<Connecting, Error>;
    fn list_members(&self) -> Result<Vec<Member>, Error>;
    async fn wait_for_updates(&mut self) -> Result<Vec<Member>, Error>;
+   fn network_name(&self) -> &str;
 }
